e011545b9b236f1463dd427f0e20cd984747c0ac
eunsongkoh <eunsongkohh@gmail.com>
2025-01-18 16:45:11 -0800

    added home page settings

--- frontend/frontend/ContentView.swift
+++ frontend/frontend/ContentView.swift
@@ -9,11 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        LiveCameraPage().tabItem{
+            Image(systemName: "house.fill")
+            Text("Home")
+        }
+        
+        CallPage().tabItem{
+            Image(systemName: "camera.fill")
+            Text("Call Page")
+        }
+        
+    }
+}
+
+struct LiveCameraPage: View {
+    var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Live Camera Mode")
+                .font(.title)
+            Button(action: {
+                print("First Page Button clicked!")
+            }) {
+                Text("Go to First Page Details")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        .padding()
+    }
+}
+
+struct CallPage: View {
+    var body: some View {
+        VStack {
+            Text("Call Mode")
+                .font(.title)
+            Button(action: {
+                print("Call Mode Page Button clicked!")
+            }) {
+                Text("Go to Second Page Details")
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
         }
         .padding()
     }
